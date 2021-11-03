d0d860281c4f5a3972d11d56fff4d8a606c01d6f
Iynga Iyngaran Iyathurai <iynga@sas.co.nz>
2021-11-03 10:44:56 +0530

    CreatePurchaseOrderItemsAction - execute - removed if before loop

--- app/Domains/PurchaseOrder/Actions/CreatePurchaseOrderItemsAction.php
+++ app/Domains/PurchaseOrder/Actions/CreatePurchaseOrderItemsAction.php
@@ -12,17 +12,15 @@ class CreatePurchaseOrderItemsAction
 {
     public function execute(array $purchaseOrderItemsData, MaterialPurchaseOrder $materialPurchaseOrder): void
     {
-        if (count($purchaseOrderItemsData) > 0) {
-            foreach ($purchaseOrderItemsData as $purchaseOrderItemData) {
-                MaterialPurchaseOrderItem::create([
-                    'material_purchase_order_id' => $materialPurchaseOrder->id,
-                    'material_variation_id' => $purchaseOrderItemData->material_variation->id,
-                    'quantity' => $purchaseOrderItemData->quantity,
-                    'unit' => $purchaseOrderItemData->unit,
-                    'price' => $purchaseOrderItemData->price,
-                    'currency' => $purchaseOrderItemData->currency
-                ]);
-            }
+        foreach ($purchaseOrderItemsData as $purchaseOrderItemData) {
+            MaterialPurchaseOrderItem::create([
+                'material_purchase_order_id' => $materialPurchaseOrder->id,
+                'material_variation_id' => $purchaseOrderItemData->material_variation->id,
+                'quantity' => $purchaseOrderItemData->quantity,
+                'unit' => $purchaseOrderItemData->unit,
+                'price' => $purchaseOrderItemData->price,
+                'currency' => $purchaseOrderItemData->currency
+            ]);
         }
     }
 }
