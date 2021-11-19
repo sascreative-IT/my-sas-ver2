eeb1b5450f4d34f31cc732b71172c3bf972504c8
Iynga Iyngaran <iynga@sas.co.nz>
2021-11-19 12:08:36 +0530

    Added currency conversion rate date to the currency

--- tests/Feature/Http/Controllers/SettingsCurrencyControllerTest.php
+++ tests/Feature/Http/Controllers/SettingsCurrencyControllerTest.php
@@ -25,10 +25,10 @@ class SettingsCurrencyControllerTest extends TestCase
 
     public function test_it_stores_properly()
     {
-        $this->withoutExceptionHandling();
-        $user = User::factory()->create();
 
+        $user = User::factory()->create();
         $this->actingAs($user);
+
         $data = [
             'name' => 'LKR',
             'currencyRateOn' => '2021-12-12',
@@ -42,6 +42,61 @@ class SettingsCurrencyControllerTest extends TestCase
             'name' => $data['name'],
             'rate' => $data['currencyRate'],
             'rate_on' => $data['currencyRateOn'],
+        ]);
+    }
+
+    public function test_it_renders_edit_page()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $currency = Currency::factory()->create();
+
+        $page = $this->get(route('settings.currencies.edit', [$currency]))
+            ->assertStatus(200)
+            ->viewData('page');
+
+        $this->assertEquals('Settings/Currency/CurrencyUpdate', $page['component']);
+        $this->assertArraysAreSimilar($currency->toArray(), $page['props']['currency']);
+    }
+
+    public function test_it_updates_properly()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $currency = Currency::factory()->create();
+
+        $data = [
+            'name' => 'LKR',
+            'currencyRateOn' => '2021-12-12',
+            'currencyRate' => '12.2134'
+        ];
+
+        $this->put(route('settings.currencies.update', $currency->id),$data)
+            ->assertStatus(302)
+            ->assertRedirect(route('settings.currencies.index'));
+
+        $this->assertDatabaseHas(Currency::class, [
+            'name' => $data['name'],
+            'rate' => $data['currencyRate'],
+            'rate_on' => $data['currencyRateOn'],
+        ]);
+    }
+
+    public function test_it_deletes_properly()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $currency = Currency::factory()->create();
+
+        $this->deleteJson(route('settings.currencies.delete', $currency))
+            ->assertStatus(302)
+            ->assertRedirect(route('settings.currencies.index'));
+
+        $this->assertDatabaseMissing(Currency::class, [
+            'name' => $currency->name,
+            'rate' => $currency->rate,
+            'rate_on' => $currency->rate_on,
         ]);
     }
 }
