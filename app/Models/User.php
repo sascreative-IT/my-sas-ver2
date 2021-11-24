d01a3e5f1474ae601e6d8f2021bf2e4a05fa4c2b
Iynga Iyngaran <iynga@sas.co.nz>
2021-11-24 12:13:11 +0530

    Modified the user CRUD with multiple factories

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -64,5 +65,10 @@ class User extends Authenticatable
     public function erpUserDetails() : HasOne
     {
         return $this->hasOne(ErpUserDetail::class, 'user_id');
+    }
+
+    public function factories(): BelongsToMany
+    {
+        return $this->belongsToMany(Factory::class, 'user_factory');
     }
 }
